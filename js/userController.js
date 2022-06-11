889354cc9835948ff7e3fcddea19d6dc28940525
Guy Kadosh <guyk50@gmail.com>
2022-06-11 18:36:48 +0300

    Added validation

--- js/userController.js
+++ js/userController.js
@@ -16,13 +16,25 @@ function renderUserColors() {
 function onSetUser(ev) {
   ev.preventDefault()
 
+  const elAge = document.querySelector('[name="age"]')
   const email = document.querySelector('[name="email"]').value
-  const age = +document.querySelector('[name="age"]').value
   const bgc = document.querySelector('[name="bgc-color"]').value
   const textColor = document.querySelector('[name="text-color"]').value
   const birthDate = document.querySelector('[name="dob"]').value
   const birthTime = document.querySelector('[name="bt"]').value
   const gender = document.querySelector('[name="gender"]').value
+
+  // Validate age match birth year
+  const age = +elAge.value
+  const currentYear = new Date().getFullYear()
+  const birthYear = birthDate.split('/')[2]
+  const ageValidate = currentYear - birthYear
+
+  if (age !== ageValidate) {
+    elAge.setCustomValidity('Your age does not match the birth year')
+    elAge.reportValidity()
+    return false
+  }
 
   const user = {
     email,
